0fa339036bfc6fb4e3bf6bf4cdc230573a37f727
sseom <ysum1011@gmail.com>
2017-01-03 23:55:32 +0900

    모달콘텐츠(레이어팝업) 및 파비콘 추가, html/css/js 파일 압축

--- la-main/js/all.js
+++ la-main/js/all.js
@@ -1,3 +1,143 @@
+/**
+ * @name		Shuffle Letters
+ * @author		Martin Angelov
+ * @version 	1.0
+ * @url			http://tutorialzine.com/2011/09/shuffle-letters-effect-jquery/
+ * @license		MIT License
+ */
+
+(function($){
+	
+	$.fn.shuffleLetters = function(prop){
+		
+		var options = $.extend({
+			"step"		: 8,			// How many times should the letters be changed
+			"fps"		: 25,			// Frames Per Second
+			"text"		: "", 			// Use this text instead of the contents
+			"callback"	: function(){}	// Run once the animation is complete
+		},prop)
+		
+		return this.each(function(){
+			
+			var el = $(this),
+				str = "";
+
+
+			// Preventing parallel animations using a flag;
+
+			if(el.data('animated')){
+				return true;
+			}
+			
+			el.data('animated',true);
+			
+			
+			if(options.text) {
+				str = options.text.split('');
+			}
+			else {
+				str = el.text().split('');
+			}
+			
+			// The types array holds the type for each character;
+			// Letters holds the positions of non-space characters;
+			
+			var types = [],
+				letters = [];
+
+			// Looping through all the chars of the string
+			
+			for(var i=0;i<str.length;i++){
+				
+				var ch = str[i];
+				
+				if(ch == " "){
+					types[i] = "space";
+					continue;
+				}
+				else if(/[a-z]/.test(ch)){
+					types[i] = "lowerLetter";
+				}
+				else if(/[A-Z]/.test(ch)){
+					types[i] = "upperLetter";
+				}
+				else {
+					types[i] = "symbol";
+				}
+				
+				letters.push(i);
+			}
+			
+			el.html("");			
+
+			// Self executing named function expression:
+			
+			(function shuffle(start){
+			
+				// This code is run options.fps times per second
+				// and updates the contents of the page element
+					
+				var i,
+					len = letters.length, 
+					strCopy = str.slice(0);	// Fresh copy of the string
+					
+				if(start>len){
+					
+					// The animation is complete. Updating the
+					// flag and triggering the callback;
+					
+					el.data('animated',false);
+					options.callback(el);
+					return;
+				}
+				
+				// All the work gets done here
+				for(i=Math.max(start,0); i < len; i++){
+
+					// The start argument and options.step limit
+					// the characters we will be working on at once
+					
+					if( i < start+options.step){
+						// Generate a random character at thsi position
+						strCopy[letters[i]] = randomChar(types[letters[i]]);
+					}
+					else {
+						strCopy[letters[i]] = "";
+					}
+				}
+				
+				el.text(strCopy.join(""));
+				
+				setTimeout(function(){
+					
+					shuffle(start+1);
+					
+				},1000/options.fps);
+				
+			})(-options.step);
+			
+
+		});
+	};
+	
+	function randomChar(type){
+		var pool = "";
+		
+		if (type == "lowerLetter"){
+			pool = "abcdefghijklmnopqrstuvwxyz0123456789";
+		}
+		else if (type == "upperLetter"){
+			pool = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+		}
+		else if (type == "symbol"){
+			pool = ",.?/\\(^)![]{}*&^%$#'\"";
+		}
+		
+		var arr = pool.split('');
+		return arr[Math.floor(Math.random()*arr.length)];
+	}
+	
+})(jQuery);
 //base.js
 
 // button event
@@ -90,7 +230,7 @@
 
 }(this, this.jQuery));
 
-// text effect
+// text effect : jquery.shuffleLetters.js
 (function(global, $){
   'use strict';
   var
@@ -176,8 +316,44 @@
     
   });
 
-
 })(this, this.jQuery);
+
+// modal 콘텐츠 : 레이어 팝업
+(function(global){
+  'use strict';
+
+  var body = document.body;
+  var modal = document.querySelectorAll(".modal");
+  var modal_num = modal.length;
+  var modal_btn = document.querySelectorAll(".modal_btn");
+  var close_btn = document.querySelectorAll(".close_btn");
+
+
+  // 순환
+  for(var i= 0; i < modal_num; i++){
+
+    var modal_open_btn = modal_btn[i];
+    modal_open_btn.num = i;
+
+    modal_open_btn.onclick = function() {
+      // console.log(this.num);
+      var index = this.num;
+      body.classList.add('modal_on');
+      modal[index].style.display = "block";
+    }
+
+    var close_con_btn = close_btn[i];
+    close_con_btn.num = i;
+
+    close_con_btn.onclick = function() {
+        var index = this.num;
+        body.classList.remove('modal_on');
+        modal[index].style.display = "none";
+    }
+
+  }
+
+})(this);
 
 
 

4ac50ede9895c250334a996cad1f272ae3fa30db
sseom <ysum1011@gmail.com>
2017-03-11 18:23:42 +0900

    Web-Font-Gallery 작업 업로드

---
+++ Web-Font-Gallery/README.md
@@ -0,0 +1,133 @@
+#Web Font Gallery 작업일지
+- [작업 페이지](https://sseom.github.io/project/Web-Font-Gallery/)
+- [작업 소스](https://github.com/sseom/project/tree/master/Web-Font-Gallery)
+- 작업자 : 윤선미(SSEOM)
+- 작업 일자 : 2017.03.10 ~ 03.11
+- E-mail : ysum1011@naver.com
+- Portfolio Blog : http://sseom.github.io/
+
+### 개발환경
+- HTML4(Transitional), CSS, CSS3, Sass, Javascript, jQuery
+- CDN 로드 : google Web Font, jQuery v1.12.4
+- Build Tool
+    - Node : v6.9.0
+    - NPM : v4.0.5
+    - Gulp : v3.9.1
+    - 커스텀마이즈한 [Sass-Gulp-module](https://github.com/sseom/project/tree/master/Web-Font-Gallery/sass-gulp-module)
+        - 사용한 plugin
+            - browser-sync : v2.18.5 
+                - 서버 환경 구성.
+            - gulp-autoprefixer : v3.1.1 
+                - CSS3 브라우저 벤더 프리픽스를 설정에 따라 자동으로 적용 처리.
+            - gulp-concat : v2.6.0 
+                - Javascript 파일 병합
+            - gulp-csscomb : v3.0.8 
+                - CSS 문서 포멧 정리 및 속성 별로 정렬
+            - gulp-if : v2.0.1 
+                - 조건에 따른 업무 처리
+            - gulp-sass : v2.3.2 
+                - sass 모듈을 로드
+            - gulp-sourcemaps : v1.6.0 
+                - 개발 시, Debug 용으로 사용할 소스맵을 생성.
+- Editor : sublimeText3
+- Test Browser : Chrom 최신버전 ( 버전 57.0.2987.98 (64-bit) )
+
+###작업 폴더 구조
+- Web-Font-Gallery : 작업 소스
+    - index.html
+    - css
+        - style.css
+    - js
+        - all.js
+    - sass
+        - _font.sass
+        - _function-mixin.sass
+        - _open-color.scss
+        - _common.sass
+        - _main-style.sass
+        - _color-table.sass
+        - style.sass
+- README.md : 작업 일지
+- sass-gulp-module.zip 
+    + package.json
+    + gulpfile.js
+    + gulp.config.js
+    + .csscomb.json
+
+###기능 구현 리스트
+- Static Web : 콘텐츠의 너비는 브라우져창의 80%이며 910px이하로는 줄어들지 않음.
+- 웹폰트 리스트에서 클릭 버튼 누르면 해당 웹폰트의 사이즈와 컬러를 미리보기 할수 있는 모달창 오픈.
+- 모달 콘텐츠
+    - 사이즈 조정
+        - 핸들링하는 아이콘을 좌우로 움직이면 웹폰트의 사이즈 변경
+    - PICK 버튼 
+        - 버튼 클릭하면 컬러칩 오픈 / 클로즈
+        - 투글기능 : 컬러칩이 오픈되면 PICK버튼의 텍스트는 CLOSE로 변경
+        - 컬러를 클릭하면 미리보기 창에세 선택 컬러가 적용된 폰트가 보임.
+    - X 버튼
+        + 모달창 클로즈.
+        + 컬러칩 클로즈.
+        + 핸들아이콘, 폰트 뷰에서의 폰트의 크기 디폴트값으로 돌아감.
+
+###Sass사용
+- _color-table.sass
+    - _open-color.scss 임폴트해서 해당 컬러와 반복문 사용해서 컬러칩이 들어갈 클래스 이름과 색상을 순서대로 뽑아냄
+- _common.sass
+    + 공통으로 사용하는 모듈.
+    + 대체선택자(Placeholder Selector, %)  사용
+- _font.sass
+    - 사용할 웹폰트 이름을 변수에 저장.
+    - 웹폰트 import할때 변수에 따라 처리.
+    - URL을 찾아서 처리하지 않고 사용하고 싶은 폰트를 변수에서 변경하거나 추가하거나 해서 사용.
+- _function-mixin.sass
+    + function : 컬러 조정하는 shade() 
+        - darken() 사용하면 원하는 색이 안나옴. 엄청 탁해짐..
+        - mix() 함수를 사용해서 black과 혼합함으로써 원하는 본래의색을 %(퍼센트)로 명암을 조절.
+        - [참고 sass-guidelin](https://sass-guidelin.es/#syntax--formatting)
+    + mixin : 위드와 하이트를 동시에 작성.
+        - [참고 sass-guidelin](https://sass-guidelin.es/#mixins)
+
+###IE8 호완 문제
+1. CSS3 
+    - border-radius : 지원하지 않음 
+        - 해결 방법 : 정보 인식에 무리는 없기 때문에 무거운 javascript 라이브러리 사용해 성능을 저하 하는것 보단 정보가 깨지지 않는 것에 중점을 둠
+    - rgba 의 알파값이 적용되지 않음
+        + 모달창이 뜨면서 모달콘텐츠 뒤로는 시각적으로 비활성화 처리했는데 적용안됨.
+        + 해결 방법 : background-color 속성과 값을 2번 작성
+        ```
+          background-color: rgb(53,49,42);
+          background-color: rgba(0,0,0,0.8);
+        ```
+    - clearfix
+        - IE8에서는 float을 적용한 콘테츠들을 감싸지 못함.
+        - 해결 방법 : 비표준속성 zoom 사용.
+2. 자바스크립트
+    - trim() 메서드 적용안됨
+        + 해결 방법 :
+            * [참고 w3schools](https://www.w3schools.com/jsref/jsref_trim_string.asp)
+            ```
+            // 방법 1.
+            // w3schools 참고
+            function myTrim(x) {
+                return x.replace(/^\s+|\s+$/gm,'');
+            }
+
+            // 방법 2.
+            // 위와같이 함수방식으로 사용하지 않고
+            // String에 프로토타입방식으로 trim 메서드 만들어서 사용
+            if(typeof String.prototype.trim !== 'function') {
+              String.prototype.trim = function() {
+                return this.replace(/^\s+|\s+$/g, ''); 
+              }
+            }
+            ```
+    - 문서의 높이값 
+        + window.innerHeight 가 적용이 안됨
+        + 해결방법 :
+            + [참고 w3schools](https://www.w3schools.com/jsref/prop_win_innerheight.asp)
+            ```
+            // w3schools 참고
+            var height = window.innerHeight
+                || document.documentElement.clientHeight
+                || document.body.clientHeight;
+            ```
---
+++ Web-Font-Gallery/js/all.js
@@ -0,0 +1,229 @@
+(function(global, $){
+  'use strict';
+
+  // ---------------[ MODAL CONTENT ]------------------
+
+  var font_article     = document.querySelectorAll('.font-article'),
+      detail_view_btn  = document.querySelectorAll('.detail-view-btn'),
+      btn_length       = detail_view_btn.length,
+      modal_wrap       = document.querySelector('.modal-wrap'),
+      modal_content    = modal_wrap.querySelector('.modal-content'),
+      modal_view_wrap  = modal_wrap.querySelector('.modal-view-wrap'),
+      modal_font_name  = modal_wrap.querySelector('.modal-font-name'),
+      detail_close_btn = modal_wrap.querySelector('.detail-close-btn');
+
+  // IE8 대응 : 문서의 높이값 가져오기
+  var height = window.innerHeight
+    || document.documentElement.clientHeight
+    || document.body.clientHeight;
+
+  // 폰트 뷰 버튼들 순환해서 이벤트 걸기
+  for(var i=0;i<btn_length;i++){
+    // 상태변수 : 각 인덱스값 저장
+    var view_btns = detail_view_btn[i];
+    view_btns.num = i;
+
+    // 폰트 뷰 버튼들 인덱스 순으로 클릭이벤트
+    detail_view_btn[i].onclick = function(e){
+      // 이벤트를 실행한 대상요소의 인덱스값 저장
+      var index = this.num;
+
+      // 대상의 폰트네임 저장후 모달 콘텐츠에 동적으로 적용
+      var font_name = font_article[index].querySelector('.font-name');
+      modal_font_name.innerText = font_name.innerText;
+      
+      // 폰트 뷰에 대상의 인덱스에 맞는 폰트클래스 적용
+      addClass(modal_view_wrap, "font-" + (index+1));
+
+      // 모달이 오픈되면서 비활성화될 부분 높이 설정
+      modal_wrap.style.height = height + 'px' ;
+      // 모달 콘텐츠 오픈
+      addClass(modal_wrap, 'modal-on');
+      // 비활성화된 부분의 스크롤을 없앰.
+      document.body.style.overflow = 'hidden';
+    };
+
+  }
+
+  // 모달 콘텐츠 클로즈
+  detail_close_btn.onclick = function(){
+    // 모달 닫힘
+    removeClass(modal_wrap, 'modal-on');
+
+    // 폰트클래스 가져와서 기존에 적용된 클래스 삭제
+    var change_class = modal_view_wrap.className;
+    change_class = change_class.charAt(change_class.length-1);
+    removeClass(modal_view_wrap, "font-" + change_class);
+    document.body.style.overflow = 'auto';
+
+    // 컬러 픽 닫힘 
+    removeClass(color_table_wrap, "pick-open");
+
+    // 핸들아이콘은 기본위치로
+    $(handring_icon).offset({left : icon_location});
+    // 아이콘의 위치에 따라 폰트도 기본으로 변경
+    var size_value = moveSize(icon_location);
+    $(modal_view_wrap).css('fontSize', size_value);
+  };
+
+// ---------------[ FONT SIZE ]------------------
+
+  var handring_icon = document.querySelector('.handring-icon'),
+      area_line     = document.querySelector('.area-line'),
+      $target       = null,
+      drag          = false;
+
+  // 아이콘 초기값 위치
+  // 0. 원래 폰트사이즈 나올때의 위치값 알아내기
+  // 1. 우선 폰트사이즈를 가져와 얼마인지
+  var size = $(modal_view_wrap).css('fontSize');
+  // 1-1. 숫자만 가져오기 : 단위제거 , 숫자로 형변환
+  // moveSize() 함수 계산식 참고 
+  // (원래폰트값 - 최소폰트값) * 분리되는단위 
+  size = (size.replace(/[^0-9]/g, '')) * 1;
+  var icon_location = (( size - 5 ) * 2 );
+  // 2. 핸들의 위치를 조정해보자 : offset 절대좌표
+  $(handring_icon).offset({left : icon_location});
+
+
+  // 아이콘 이벤트 : 마우스 버튼을 누를 때
+  $(handring_icon).on('mousedown', function(evt){
+  
+    $target = $(this);
+    drag = true;
+
+    // 아이콘 움직임 이벤트 : 마우스를 움직일 때
+    $(area_line).on('mousemove', function(evt){
+      // 마우스 위치 == 클릭한 영역
+      var changeX = evt.clientX;
+      // 클릭한 icon 위치를 마우스 위치로 설정
+      $target.offset({ left:changeX });
+
+      // 아이콘 움직임에 따라 폰트에 대입할 사이즈값 가져오기
+      var size_value = moveSize(changeX);
+      // 폰트사이즈 설정
+      $(modal_view_wrap).css('fontSize', size_value);
+    });
+
+  });
+
+  // 아이콘 정지 이벤트 : 마우스 버튼을 뗄 때 발생
+  $(handring_icon).on('mouseup',function(evt){
+      // 초기화
+      drag = false;
+      $target = null;
+      $(area_line).off('mousemove');
+  });
+
+
+  // ------------------ 함수 ------------------ 
+
+  // 움직인만큼 폰트 사이즈 값반환
+  function moveSize(x){
+    // 핸들링이 움직이는 라인의 
+    // 예를들어 (환경마다 다름)
+    // 영역은 대략 303~430 정도의 움직임이 보임 : 130단계정도
+    // 라인의 위치는 303
+    // 움직임의 위치 - 라인의 시작점위치 
+    var num = x - $(area_line).offset().left;
+    // 5 => 폰트 최소값 , 2 => 영역 분리되는 단위
+    var val = (num/2) + 5;
+    // 폰트값 소숫점 안나오게 소숫점 절삭.
+    return Math.floor(val);
+  }
+
+
+
+  // ---------------[ COLOR PICK ]------------------
+
+  // PICK 버튼 클릭 이벤트
+  var color_btn        = document.querySelector('.color-btn'),
+      color_table_wrap = document.querySelector('.color-table-wrap'),
+      color_table      = document.querySelector('.color-table'),
+      color_ol         = color_table.querySelectorAll('.color-ol');
+
+
+  color_btn.onclick = function(){
+    var btn_val   = "CLOSE",
+        btn_state = this.value.indexOf('CLOSE') > -1;
+
+    // false(-1 나오면) 라면
+    if(!btn_state){
+      // 버튼값은 클로즈
+      color_btn.value = btn_val;
+      // 컬러칩은 오픈
+      addClass(color_table_wrap, "pick-open");
+    }else{
+      // 버튼은 픽으로
+      btn_val = "PICK";
+      color_btn.value = btn_val;
+      // 컬러칩은 닫힘
+      removeClass(color_table_wrap, "pick-open");
+    }
+  };
+
+
+  // 각 컬러들 대상 찾아서 colorEvent 함수 연결
+  for(var i=0;i<color_ol.length;i++){
+    var color_lists = color_ol[i].querySelectorAll('li'),
+        color_num   = color_lists.length;
+
+    colorEvent(color_lists);
+  }
+
+  // 각 컬러마다 이벤트 설정
+  function colorEvent(list){ 
+    for(var i=0;i<color_num;i++){
+      list[i].onclick = colorValue;
+    }
+  }
+
+  // 컬러값 뽑아내서 클릭하면 미리보기 텍스트에 컬러 설정
+  function colorValue(){
+    var $this_el  = $(this),
+        color_val = $this_el.css('backgroundColor');
+
+    modal_view_wrap.style.color = color_val;
+  }
+
+
+  // ---------------[ HELPPER FUNCTION ]------------------
+
+  // 클래스 추가 
+  function addClass(el, class_name) {
+    // 순환문에서 사용할때 클래스가 계속 추가되는 현상 방지
+    // 클래스에 내가 추가하려는 클래스명이 있는지 확인후 
+    var is_showing = el.className.indexOf(class_name) > -1;
+    // 있으면 실행말고 나가
+    if( is_showing ){ return; }
+    // 없으니까 추가돼
+    // " " 공백 넣어서 클래스 추가 
+    el.className += " " + class_name;
+  }
+
+  // 클래스 제거
+  function removeClass(el, class_name) {
+    // 정규식표현 : 문자 앞 뒤 부분 공백 한개씩
+    var ck = new RegExp( "(\\s|^)"  +  class_name  + "(\\s|$)");
+    // myTrim() 사용해서 단어의 앞과 뒤의 공백지움. 
+    var trim_val = el.className.replace( ck , ' ');
+    el.className = myTrim(trim_val);
+  }
+
+  // trim() 최신, ie9 이상 지원 : ie8 support
+  function myTrim(x) {
+    return x.replace(/^\s+|\s+$/gm,'');
+  }
+
+  // 투글 클래스
+  function toggleClass(el, class_name) {
+    // indexOf()를 사용해서 클래스가 있는지 확인함. 불리언값을 반환
+    var is_showing = el.className.indexOf(class_name) > -1;
+    if( is_showing ){
+      removeClass(el, class_name) 
+    }else{
+      addClass(el, class_name)
+    }
+  }
+
+})(this, jQuery);
